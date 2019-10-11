dfd0ea973a97dabbb4aef9abcf099f4e6dc79c4d
Fernando Yamanaka <fernandomineo@gmail.com>
2019-10-10 22:44:37 -0300

    Start DOcker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
-ARG DEPENDENCY=target/dependency
-COPY ${DEPENDENCY}/BOOT-INF/lib /app/lib
-COPY ${DEPENDENCY}/META-INF /app/META-INF
-COPY ${DEPENDENCY}/BOOT-INF/classes /app
-ENTRYPOINT ["java","-cp","app:app/lib/*","hello.Application"]
+
+COPY target/htmlwrapper-0.1-SNAPSHOT.jar htmlwrapper-0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","htmlwrapper-0.1-SNAPSHOT.jar"]
--- src/test/unittest/WrapperControllerTest.java
+++ src/test/unittest/WrapperControllerTest.java
@@ -1,12 +1,19 @@
-package unittest;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import wrapper.WrapperApplication;
 
 import static org.junit.Assert.*;
 
@@ -166,5 +173,36 @@ public class WrapperControllerTest extends AbstractWrapperTest {
         }
         return true;
     }
-
 }
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = WrapperApplication.class)
+@WebAppConfiguration
+class AbstractWrapperTest {
+    protected MockMvc mvc;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    protected void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    protected static String EMPTY_HOST = "";
+    protected static String INCOMPLETE_HOST = "http://www.";
+    protected static String UNKNOWN_HOST = "http://www.terrrraa.com.br";
+    protected static String NUMBER_PORT_HOST = "http://216.58.202.206:80"; // google.com host
+    protected static String AMAZON_HOST = "http://www.amazon.com.br"; // Page with no <a href> lings
+
+    // Short list ~ 15 Links in page
+    protected static String GOOGLE_HOST = "http://www.google.com.br";
+    // Large list ~ 450 links in page
+    protected static String UOL_PORTAL_HOST = "http://www.uol.com.br";
+
+    // Exploratory Tests = 4 links
+    protected static String EXPL_HOST1 = "http://www.terra.com.br";
+    protected static String EXPL_HOST2 = "http://www.yahoo.com.br";
+    protected static String EXPL_HOST3 = "http://www.meutimao.com.br";
+    protected static String EXPL_HOST4 = "http://www.milliondollarhomepage.com/";
+}
+
